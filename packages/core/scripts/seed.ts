f12a02ad9c0a53a76d19535f66d75b521edbfd1d
Carlo <hola.carlodominguez@gmail.com>
2025-04-20 13:53:34 +0200

    chore: add logs to `seed` script

--- packages/core/scripts/seed.ts
+++ packages/core/scripts/seed.ts
@@ -2,4 +2,8 @@ import { seed } from "drizzle-seed"
 import { database } from "@/database/client"
 import * as schema from "@/database/schema"
 
+console.log("🌱 Seeding database…")
+
 await seed(database, schema)
+
+console.log("✅ Seeded database")
